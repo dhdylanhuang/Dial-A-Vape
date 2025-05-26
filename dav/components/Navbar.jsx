f9fa4256dd95cc9a29896078ee12f03f9bd834e6
2789356h <2789356h@student.gla.ac.uk>
2025-05-26 04:27:52 +0400

    Cart Option

--- dav/components/Navbar.jsx
+++ dav/components/Navbar.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React from "react";
-import { assets } from "@/assets/assets";
+import { assets, CartIcon } from "@/assets/assets";
 import Link from "next/link"
 import { useAppContext } from "@/context/AppContext";
 import Image from "next/image";
@@ -42,7 +42,11 @@ const Navbar = () => {
         {
           user 
           ? <>
-          <UserButton />
+          <UserButton>
+            <UserButton.MenuItems>
+              <UserButton.Action label = "Cart" labelIcon={<CartIcon />} onClick={() => router.push('/cart')} />
+            </UserButton.MenuItems>
+          </UserButton>
           </> 
           :  <button onClick={openSignIn} className="flex items-center gap-2 hover:text-gray-900 transition">
               <Image src={assets.user_icon} alt="user icon" />
