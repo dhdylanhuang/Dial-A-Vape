46b8254df1cc1bf935569d10fff87f4a98f44d5b
2789356h <2789356h@student.gla.ac.uk>
2025-05-27 21:15:47 +0200

    Demo Sliding Banner

--- dav/components/HeaderSlider.jsx
+++ dav/components/HeaderSlider.jsx
@@ -6,27 +6,27 @@ const HeaderSlider = () => {
   const sliderData = [
     {
       id: 1,
-      title: "Experience Pure Sound - Your Perfect Headphones Awaits!",
+      title: "Check out our latest vape collection - Dial A Vape!",
       offer: "Limited Time Offer 30% Off",
       buttonText1: "Buy now",
       buttonText2: "Find more",
-      imgSrc: assets.header_headphone_image,
+      imgSrc: assets.vape1,
     },
     {
       id: 2,
-      title: "Next-Level Gaming Starts Here - Discover PlayStation 5 Today!",
+      title: "Discover the Best Snus Products - Dial A Vape!",
       offer: "Hurry up only few lefts!",
       buttonText1: "Shop Now",
       buttonText2: "Explore Deals",
-      imgSrc: assets.header_playstation_image,
+      imgSrc: assets.snus1,
     },
     {
       id: 3,
-      title: "Power Meets Elegance - Apple MacBook Pro is Here for you!",
+      title: "Experience the Finest Vape Salts - Dial A Vape!",
       offer: "Exclusive Deal 40% Off",
       buttonText1: "Order Now",
       buttonText2: "Learn More",
-      imgSrc: assets.header_macbook_image,
+      imgSrc: assets.salts1,
     },
   ];
 
